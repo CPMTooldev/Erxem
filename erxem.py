3114ba973d0c15326aaa28293e0cb4d04d237199
CPMTooldev <telmnbattr@gmail.com>
2025-03-30 22:54:38 +0800

    Update and rename main (1).py to erxem.py

--- erxem.py
+++ erxem.py
@@ -11,10 +11,10 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 
-from yuxin import CPMTooldev
+from erxem import CPMTooldev
 
-__CHANNEL_USERNAME__ = "YuxinStore"
-__GROUP_USERNAME__   = "YuxinStoreChat"
+__CHANNEL_USERNAME__ = "ErxemShopChannel"
+__GROUP_USERNAME__   = "Erxem"
 
 def signal_handler(sig, frame):
     print("\n Bye Bye...")
@@ -162,7 +162,7 @@ if __name__ == "__main__":
             load_player_data(cpm)
             load_key_data(cpm)
             load_client_details()
-            choices = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28"]
+            choices = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26"]
             print(Colorate.Horizontal(Colors.rainbow, '{01}: Increase Money           1.5K'))
             print(Colorate.Horizontal(Colors.rainbow, '{02}: Increase Coins           4.5K'))
             print(Colorate.Horizontal(Colors.rainbow, '{03}: King Rank                8K'))
@@ -188,9 +188,7 @@ if __name__ == "__main__":
             print(Colorate.Horizontal(Colors.rainbow, '{23}: Unlock Equipaments F     3K'))
             print(Colorate.Horizontal(Colors.rainbow, '{24}: Change Race Wins         1K'))
             print(Colorate.Horizontal(Colors.rainbow, '{25}: Change Race Loses        1K'))
-            print(Colorate.Horizontal(Colors.rainbow, '{26}: Clone Account            7K'))
-            print(Colorate.Horizontal(Colors.rainbow, '{27}: Glitch car               2.5k'))
-            print(Colorate.Horizontal(Colors.rainbow, '{28}: Custom Angle             1.5k'))
+            print(Colorate.Horizontal(Colors.rainbow, '{26}: Clone Account            7K'))    
             print(Colorate.Horizontal(Colors.rainbow, '{0} : Exit'))
             
             print(Colorate.Horizontal(Colors.rainbow, '===============[ 𝐂𝐏𝐌 ]==============='))
